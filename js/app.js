69bdd92b2086fa350e4de1b6a63dcc91fc8192af
Avi Aryan <avi.aryan123@gmail.com>
2018-01-23 12:37:12 +0530

    Generate cards using JS

--- js/app.js
+++ js/app.js
@@ -1,18 +1,13 @@
 /*
- * Create a list that holds all of your cards
+ * App's JavaScript code
  */
 
+const cardList = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -36,3 +31,20 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+function createCard(cardClass){
+    $("ul.deck").append(`<li class="card"><i class="fa ${cardClass}"></i></li>`);
+}
+
+function populateCards(){
+    // const cl1 = shuffle(cardList);
+    shuffle(cardList).forEach(createCard);
+    // const cl2 = shuffle(cardList);
+    shuffle(cardList).forEach(createCard);
+}
+
+// start the game
+$(document).ready(function (){
+    populateCards();
+});
